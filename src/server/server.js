31955b54560b64b2579bd2dcd0e77aaa62214694
Brit Lim <britsta92@gmail.com>
2022-01-10 12:37:12 -0800

    initial boilerplate Co-authored-by: Nick Echols <Nechols87@users.noreply.github.com> Co-authored-by: TylerPohn <TylerPohn@users.noreply.github.com> Co-authored-by: Alexis <lexlexsquared@users.noreply.github.com>

---
+++ src/server/server.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const { fsync } = require('fs');
+const PORT = 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('assets'));
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+});
+
+//global error handler
+app.use('/', (err, req, res, next) => {
+  const defaultError = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'Internal Server Error' },
+  };
+  const errorObject = Object.assign({}, defaultError, err);
+  console.log(errorObject.log);
+  return res.status(errorObject.status).json(errorObject.message);
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
