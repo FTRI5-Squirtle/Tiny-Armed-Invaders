cdd955a4d2c795e88da393cd89822078d682057e
Alexis <sixela295@gmail.com>
2022-01-10 20:46:20 -0500

    added component structure for game + notes

---
+++ src/components/UserScore.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// this will contain a view of the user's current score that increments
+// this will contain short instructions on how to play the game
+// this will show current level, elapsed time
+
+export default UserScore;
