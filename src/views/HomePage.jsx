2aa32cda8ae0489db03206232936936e4ec84910
Alexis <sixela295@gmail.com>
2022-01-10 20:12:42 -0500

    updated dependencies

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Login from './Login.jsx';
 import SignUp from './SignUp.jsx';
 
